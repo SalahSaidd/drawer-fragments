8f5556af497a8db1883d63d3cff6e8098f463dc6
SalahSaidd <ssalahuddinsaid@gmai.com>
2018-10-03 20:03:45 +0800

    Laboratory Pass

---
+++ app/src/main/java/com/drawer/said/drawer_layout_template/TextFieldDemoFragment.java
@@ -0,0 +1,20 @@
+package com.drawer.said.drawer_layout_template;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TextFieldDemoFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.textfield_demo_layout, container, false);
+
+        return layout;
+    }
+}
